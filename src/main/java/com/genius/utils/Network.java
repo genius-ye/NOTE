d6a626af74d6823189936153a2fcc906bf0db8a1
genius-ye <505661829@qq.com>
2016-03-09 15:46:01 +0800

    first commit @2016.3.9

---
+++ src/main/java/com/genius/utils/Network.java
@@ -0,0 +1,20 @@
+package com.genius.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class Network {
+	public static boolean isWifi(Context context) {
+		ConnectivityManager connManager = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo mWifi = connManager
+				.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+		if (mWifi.isConnected()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
